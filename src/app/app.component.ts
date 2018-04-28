51393a3c9a81a7ad16c4aed5508aa330b4fcef70
Daniel Villalba <villalbaweb@gmail.com>
2018-04-28 18:24:27 -0500

    Adding initial animations setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, trigger, state, style } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  animations: [
+    trigger('divState', [   // this is the defined trigger name that is bound to the template element
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px)'
+      }))
+    ])
+  ]
 })
 export class AppComponent {
+  state = 'normal';  // this property is used to handle the initial state
   list = ['Milk', 'Sugar', 'Bread'];
 
     onAdd(item) {
       this.list.push(item);
+    }
+
+    onDelete(item) {
+
     }
 }
