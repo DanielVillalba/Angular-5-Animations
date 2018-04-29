4795ab1c5907fec5dac385d1500c6c84d210c74b
Daniel Villalba <villalbaweb@gmail.com>
2018-04-28 19:06:40 -0500

    Adding a second element and define the use of the two way transition and wildcard char

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,18 +20,42 @@ import { transition } from '@angular/animations';
         backgroundColor: 'blue',
         transform: 'translateX(100px)'
       })),
-      transition('normal => highlighted', animate(300)),  // explicitelly indicate the state transition 'normal => highlighted'
-      transition('highlighted => normal', animate(3000))
+      transition('normal <=> highlighted', animate(300)),  // explicitelly indicate the state transition 'normal => highlighted'
+      // transition('highlighted => normal', animate(3000))
+      // if the speed is the same fro both directions we can use <=> as defined
+    ]),
+    trigger('wildState', [   // this is the defined trigger name that is bound to the template element
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        backgroundColor: 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        backgroundColor: 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
+      transition('normal <=> highlighted', animate(300)),  // explicitelly indicate the state transition 'normal => highlighted'
+      transition('highlighted => normal', animate(3000)),
+      transition('shrunken <=> *', animate(500))  // we can use the '*' wildcard character to define any state
     ])
   ]
 })
 export class AppComponent {
   state = 'normal';  // this property is used to handle the initial state
+  wildState = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
-  onAnimate() {
-    this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
-  }
+    onAnimate() {
+      this.state === 'normal' ? this.state = 'highlighted' : this.state = 'normal';
+      this.wildState === 'normal' ? this.wildState = 'highlighted' : this.wildState = 'normal';
+    }
+
+    onShrink() {
+      this.wildState = 'shrunken';
+    }
     onAdd(item) {
       this.list.push(item);
     }
