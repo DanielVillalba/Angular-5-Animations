86f532cd8ff87b5fc9ab934dfce9d9768238f619
Daniel Villalba <villalbaweb@gmail.com>
2018-04-28 19:28:59 -0500

    Adding a more complex transition where we can define an array of phases or stages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -27,19 +27,33 @@ import { transition } from '@angular/animations';
     trigger('wildState', [   // this is the defined trigger name that is bound to the template element
       state('normal', style({
         'background-color': 'red',
-        transform: 'translateX(0) scale(1)'
+        transform: 'translateX(0) scale(1)',
+        borderRadius: '0'
       })),
       state('highlighted', style({
         backgroundColor: 'blue',
-        transform: 'translateX(100px) scale(1)'
+        transform: 'translateX(100px) scale(1)',
+        borderRadius: '0'
       })),
       state('shrunken', style({
         backgroundColor: 'green',
-        transform: 'translateX(0) scale(0.5)'
+        transform: 'translateX(0) scale(0.5)',
+        borderRadius: '0'
       })),
-      transition('normal <=> highlighted', animate(300)),  // explicitelly indicate the state transition 'normal => highlighted'
-      transition('highlighted => normal', animate(3000)),
-      transition('shrunken <=> *', animate(500))  // we can use the '*' wildcard character to define any state
+      transition('normal => highlighted', animate(300)),  // explicitelly indicate the state transition 'normal => highlighted'
+      transition('highlighted => normal', animate(300)),
+      // in the following transition we are defining an array of ohases
+      // that will animate the entire switch state
+      // we can use the '*' wildcard character to define any state
+      transition('shrunken <=> *', [
+        style( {  // no animate function so instantly apply the style
+          backgroundColor: 'orange'
+        }),
+        animate(500, style({  // here animate and a style so it will take 500 ms to completelly apply the style
+          borderRadius: '50px'
+        })),
+        animate(500)  // animate without style simply takes 500 ms to complete transition to final state
+      ])
     ])
   ]
 })
